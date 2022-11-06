6ff8b359d3aeda48cae737f3f36ed32dd0aaeca9
mlaudu-training <117585901+mlaudu-training@users.noreply.github.com>
2022-11-06 16:48:04 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
-  key_name      = "devops-dirane"
   tags = {
     Name = "ec2-dirane"
   }
